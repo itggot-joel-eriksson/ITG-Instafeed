3e92f03c3e86bb6aef25de6a799b67f7454ec693
itggot-joel-eriksson <joel.eriksson3@itggot.se>
2016-03-03 20:48:19 +0100

    Tweaks and changes to UI and functionality
    Increased base font size
    Added more images to the slideshow
    Added more colors
    Added a clock
    Added profile picture
    Added footer

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
-var postLimit = 5,
+var postLimit = 7,
     color = 0,
     instaInterval,
+    clockInterval,
     wait = 10,
     colors = [
-        "#1A8CCD",
-        "#DC006E",
-        "#EE822F",
-        "#9C27B0",
-        "#4CAF50"
+        "rgb(239,83,80)",
+        "rgb(255,202,40)",
+        "rgb(26,140,205)",
+        "rgb(220,0,110)",
+        "rgb(238,130,47)",
+        "rgb(156,39,176)",
+        "rgb(76,175,80)"
     ];
 
 String.prototype.linkify = function() {
@@ -23,10 +26,12 @@ function getInstafeed() {
         limit: postLimit,
         target: "feed",
         resolution: "standard_resolution",
-        template: '<div class="post post--hidden animated section group"><div class="col span_1_of_2"><img src="{{image}}" alt="{{model.user.username}}" draggable="false" class="post__image" /></div><div class="col span_1_of_2"><p class="post__caption">{{caption}}</p></div></div>',
+        template: '<div class="post post--hidden animated" data-insta-profile="{{model.user.username}}" data-insta-avatar="{{model.user.profile_picture}}"><img src="{{image}}" alt="{{model.user.username}}" draggable="false" class="post__image" /><p class="post__caption">{{caption}}</p></div>',
         after: function() {
+            $(".post:not(.post:nth(0))").hide();
+
             $(".post__caption").each(function(current) {
-                caption = $(this).text();
+                caption = $(this).html();
                 $(this).html(caption.linkify());
             });
 
@@ -45,7 +50,7 @@ function do_urls() {
     }
 }
 
-url_interval = setInterval(do_urls, (wait * 1000));
+var url_interval = setInterval(do_urls, (wait * 1000));
 $("body").css("background-color", colors[0]);
 
 function showPost(n) {
@@ -58,6 +63,9 @@ function showPost(n) {
     setTimeout(function() {
         $(".post:not(.post:nth(" + n + "))").hide();
         $(".post:nth(" + n + ")").show().removeClass(hideAnimation).addClass(showAnimation).addClass("post--active");
+        $(".account__name").text($(".post:nth(" + n + ")").attr("data-insta-profile"));
+        $(".account__avatar").attr("alt", $(".post:nth(" + n + ")").attr("data-insta-profile"));
+        $(".account__avatar").attr("src", $(".post:nth(" + n + ")").attr("data-insta-avatar"));
     }, 300);
 
     $(".body").css("background-color", colors[color]);
@@ -69,5 +77,13 @@ function showPost(n) {
 
 $(document).ready(function() {
     getInstafeed();
-    instaInterval = setInterval(getInstafeed, 120 * 1000);
+    instaInterval = setInterval(getInstafeed, 3 * 60 * 1000);
+
+    $(".time__now--hour").text(moment().locale("sv").format("HH"));
+    $(".time__now--minute").text(moment().locale("sv").format("mm"));
+
+    clockInterval = setInterval(function() {
+        $(".time__now--hour").text(moment().locale("sv").format("HH"));
+        $(".time__now--minute").text(moment().locale("sv").format("mm"));
+    }, 1000);
 });
